42e3d4d969ef29d1d81c79dd5da3c6505b6471ac
Lutif <lutif.ali@syntaxsquad.com>
2020-12-08 19:48:55 +0500

    adds findNotLinkedYet function to find users not linked

--- linked.js
+++ linked.js
@@ -1,2 +1,14 @@
 // find all the users that have not been linked
-//some changes
+const users = require('./users')
+
+const { linked, allUsers} = users
+
+const findNotLinkedYet = (allUsers, linked) =>{
+    
+    const linkedUserIds = linked.map(user => user.id)
+    const notLinkedYet = allUsers.filter( user => !linkedUserIds.includes(user.id) )
+    return notLinkedYet
+}
+
+
+console.log(findNotLinkedYet(allUsers, linked))
